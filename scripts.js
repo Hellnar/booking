3bc746bb06e106f3e4fb27ba16ffd7e2a41bbb4f
Stas Klymenko <klymenko.stas@gmail.com>
2020-06-22 12:00:15 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,20 +1,31 @@
 function calendar() {
 	document.getElementById('content-calendar').style.display = "flex";
+	document.getElementById('content-book-meeting').style.display = "none";
 	document.getElementById('content-booked').style.display = "none";
 	document.getElementById('content-tasks').style.display = "none";
 }
 
 function meetings() {
 	document.getElementById('content-calendar').style.display = "none";
+	document.getElementById('content-book-meeting').style.display = "none";
 	document.getElementById('content-booked').style.display = "flex";
 	document.getElementById('content-tasks').style.display = "none";
 }
 
 function tasks() {
 	document.getElementById('content-calendar').style.display = "none";
+	document.getElementById('content-book-meeting').style.display = "none";
 	document.getElementById('content-booked').style.display = "none";
 	document.getElementById('content-tasks').style.display = "flex";
 }
+
+function bookMeeting() {
+	document.getElementById('content-calendar').style.display = "none";
+	document.getElementById('content-book-meeting').style.display = "flex";
+	document.getElementById('content-booked').style.display = "none";
+	document.getElementById('content-tasks').style.display = "none";
+}
+
 
 document.querySelector('.tasks-completed').style.display = "none";
 document.getElementById('show-completed').innerText = "Show completed tasks";
